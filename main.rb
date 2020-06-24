d798227b987108586175798b78f4a91195b98503
lunglagael <lunglagael@gmail.com>
2020-06-24 01:18:44 +0100

    updates

--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def fibs(number)
     fib_array = [0]
     (number).times {fib_array.size === 1 ? fib_array.push(1) : fib_array.push(fib_array.last(2).sum)}
@@ -8,3 +9,4 @@ def fibs_rec(number,series)
     return series if number.eql?(series.size)
     series.size === 1 ? fibs_rec(number,series.push(1)) : fibs_rec(number,series.push(series.last(2).sum))
 end
+binding.pry
